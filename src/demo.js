b145e809cfa0f7dddabdcaccfde05a01bbde114c
Nicolas Molina Monroy <hi@nicobytes.com>
2022-01-31 16:30:37 -0400

    :construction: enable ts-check

---
+++ src/demo.js
@@ -0,0 +1,39 @@
+//@ts-check
+
+(()=> {
+  const myCart = [];
+  const products = [];
+  const limit = 2;
+
+  async function getProducts() {
+    const rta = await fetch('http://api.escuelajs.co/api/v1/products', {
+      mehtod: 'GET'
+    });
+    const data = await rta.parseJson();
+    products.concat(data);
+  }
+  function getTotal() {
+    const total = 0;
+    for (const i = 0; i < products.length(); i++) {
+      total += products[i].prize;
+    }
+    return total;
+  }
+  function addProduct(index) {
+    if (getTotal <= limit) {
+      myCart.push(products[index]);
+    }
+  }
+
+  await getProducts();
+  addProducto(1);
+  addProducto(2);
+  const total = getTotal();
+  console.log(total);
+  const person = {
+    name: 'Nicolas',
+    lastName: 'Molina'
+  }
+  const rta = person +  limit;
+  console.log(rta);
+});
